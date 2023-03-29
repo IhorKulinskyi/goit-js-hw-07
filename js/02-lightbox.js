05d91c99c765f58d0ae449b749616bce03af9eec
IhorKulinskyi <110032104+IhorKulinskyi@users.noreply.github.com>
2023-03-29 13:52:50 +0200

    cdn

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+galleryList.innerHTML = makeGalleryItemsMarkup(galleryItems);
+
+galleryList.addEventListener("click", openModalWindow);
+
+function openModalWindow(event) {
+  event.preventDefault();
+
+  if (event.target.classList.value !== "gallery__image") {
+    return;
+  }
+
+  new SimpleLightbox(".gallery a", {
+    captionType: "attr",
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+  });
+}
+
+function makeGalleryItemsMarkup(galleryItems) {
+  const markup = galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+           <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        </li> `;
+    })
+    .join("");
+
+  return markup;
+}
